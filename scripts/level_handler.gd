27a6a0f24ad1417e3b6b4041085ba3619482a5ac
Norrolith <unwary@earthlink.net>
2016-06-12 12:46:30 -0500

    Added menu kick out, menu fly in

--- scripts/level_handler.gd
+++ scripts/level_handler.gd
@@ -9,12 +9,14 @@ func _process(delta):
 	if (score_keep.eggs_dropped > score_keep.lives):
 		#Load the start over or main menu when those are implemnted.
 		#for now do the following
-		print("Ofeateo")
-		score_keep.player_score=0
-		score_keep.eggs_dropped=0
-		score_keep.lives=3
+		reset_level()
 		#These would clean up the splats
 		#var splat_node=get_node("../splatter_handler")
 		#if (splat_node!==no
 
-
+func reset_level():
+	print("Ofeateo")
+	score_keep.player_score=0
+	score_keep.eggs_dropped=0
+	score_keep.lives=3
+	get_tree().change_scene("res://scenes/menu.scn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,6 @@
 
 extends Node2D
-
+var final_pos= Vector2(0,0)
 # member variables here, example:
 # var a=2
 # var b="textvar"
@@ -8,10 +8,19 @@ extends Node2D
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
-
-
-
+	final_pos=get_pos()
+	var new_pos=final_pos
+	new_pos.y-=550
+	set_pos(new_pos)
+	set_process(true)
+	
+func _process(delta):
+	var new_pos=get_pos()
+	new_pos.y=new_pos.y+15
+	if (!new_pos>=final_pos):
+		set_pos(new_pos)
+	else:
+		set_process(false)
 
 func _on_start_input_event( ev ):
 	if (ev.is_pressed()):
